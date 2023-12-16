f581ce2f416571115e4ad9debd068ee29813f9a0
Charles M. Wolfe <cmwolfe.dev@gmail.com>
2023-12-16 10:08:35 -0600

    Started on the frontend. Deleted a lot of the react boilerplate code. Added the axios package for the ApiService.js file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,19 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # Added all these apps to the settings file.
-    'rest_framework',
-    'api_manager',
-    'corsheaders',
-    'bootstrap5',
-    'crispy_forms',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Added all these apps to the settings file.
+    'rest_framework',
+    'api_manager',
+    'corsheaders',
 ]
 
 # Added this for the corsheaders package
